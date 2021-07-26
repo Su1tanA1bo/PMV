bfea26ea14bc0045d2ce169e9a646893aee5b9ef
Su1tanA1bo <Sultanalbo06@gmail.com>
2021-07-26 23:09:25 +0100

    mk3

--- mk3.py
+++ mk3.py
@@ -18,12 +18,16 @@ print("Logged In")
 balances = info['balances']
 targetCoinExchange = "ETHUSDT"
 
+#Seeing if price is correct
+ETH = client.get_symbol_ticker(symbol="ETHUSDT")
+print("Beginning program at " + (ETH["price"]))
+
 def GetHistoricalPrices(targetCoinExchange):
     TargetPrices = client.get_historical_klines(targetCoinExchange, Client.KLINE_INTERVAL_1DAY, "1 Sep, 2017")
     return TargetPrices
 
 def WritetoCSV(TargetPrices):
-        with open('CurrencyPrices2.csv', 'w') as d:
+        with open('CurrencyPrices.csv', 'w') as d:
             for line in TargetPrices:
                 d.write(f'{line[0]}, {line[1]}, {line[2]}, {line[3]}, {line[4]}\n')
 
@@ -48,5 +52,46 @@ signal = MACD.ewm(span=5, adjust=False).mean()
 targetDf['MACD'] = MACD
 targetDf['Signal'] = signal
 
-#for(x )
+print(targetDf)
+
+#BuySellSignal will return 1/0 depending on whether MACD suggest buy/sell
+def BuySellSignal(recentMACDValue, recentSignalValue):
+    posOnGraph = " "
+    if (float(recentMACDValue)) >= 0 and float(recentSignalValue) >= 0:
+        posOnGraph="bothPositive"
+    elif (float(recentMACDValue)) >= 0 and float(recentSignalValue) <= 0:
+        posOnGraph ="macdPositive,signalNegative"
+    elif (float(recentMACDValue)) <= 0 and float(recentSignalValue) >= 0:
+        posOnGraph="macdNegative,signalPositive"
+    elif (float(recentMACDValue)) <= 0 and float(recentSignalValue) <= 0:
+        posOnGraph="bothNegative"
+    
+    if posOnGraph == "bothPositive":
+        if((recentMACDValue - recentSignalValue)):
+            return 1
+        else: 
+            return 0
+
+    elif posOnGraph == "macdPositive,signalNegative":
+        if((recentMACDValue) - (recentSignalValue) >= 3):
+            return 1
+        else:
+            return 0
+
+    elif posOnGraph == "macdNegative,signalPositive":
+        return 0
+
+    elif posOnGraph == "bothNegative":
+        if(abs(recentSignalValue) - abs(recentMACDValue) > 0):
+            return 1
+        else:
+            return 0
+
+
+def DataframeCycler(df):
+   # while
+        
+
+
+DataframeCycler(targetDf)
 
